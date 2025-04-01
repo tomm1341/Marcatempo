dfad8fb04e2ccc33abc39c27faacd28743d201ff
tomm1341 <tommaso.foca@studio.unibo.it>
2025-04-01 17:35:27 +0200

    Created Commessa.cs

---
+++ Template/Services/Shared/Commessa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Template.Services.Shared
+{
+    internal class Commessa
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+
+    }
+}
