920e08403713ba7e56d71a76631fed65dee1e301
tomm1341 <tommaso.foca@studio.unibo.it>
2025-04-01 17:25:10 +0200

    Created Tasks.md

---
+++ Template/Services/Shared/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Template.Services.Shared
+{
+    internal class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public Guid IdCommessa { get; set;}
+
+        public String Stato { get; set;} //In lavorazione, completato ecc...
+        public String Titolo { get; set;}
+        public String Descrizione { get; set;}
+
+
+    }
+}
