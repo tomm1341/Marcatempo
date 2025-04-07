c6cb5689870a55b4e0294cf72ee1740cc3fa2b07
tomm1341 <tommaso.foca@studio.unibo.it>
2025-04-07 17:39:00 +0200

    Created DataCreazione and DataScadenza variables

--- Template/Services/Shared/Task.cs
+++ Template/Services/Shared/Task.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -12,13 +13,17 @@ namespace Template.Services.Shared
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
-        public Guid IdCommessa { get; set;} //FK
+        public Guid IdCreatore { get; set;} // FK -> Utente
 
         [Required]
         [StatoValido(new[] { "InAttesa", "InLavorazione", "Completato", "Approvato", "Respinto" })]
         public string Stato { get; set; }
         public string Titolo { get; set;}
         public string Descrizione { get; set;}
+
+        public DateTime DataCreazione { get; set;} // Optional
+
+        public DateTime DataScadenza { get; set;} // Optional
 
 
     }
