1252b59da8cd3393a96d1efa4b57026cffe78af3
AlexApo03 <alexandru.apostol@studio.unibo.it>
2025-04-14 10:02:31 +0200

    Impostazione grafica e prima logica pagina storico task completati

--- Template.Web/Features/Storico/StoricoController.cs
+++ Template.Web/Features/Storico/StoricoController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Template.Services;
 using System;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace Template.Web.Features.Storico
 {
@@ -21,67 +22,69 @@ namespace Template.Web.Features.Storico
         [HttpGet]
         public async virtual Task<IActionResult> Storico()
         {
-            var userEmail = User.Identity.Name; // Get user email
+            //var userEmail = User.Identity.Name; // Get user email
 
-            if (string.IsNullOrEmpty(userEmail))
-            {
-                Console.WriteLine("User.Identity.Name è null o vuoto");
-            }
-            else
-            {
-                Console.WriteLine($"User.Identity.Name: {userEmail}");
-            }
+            //if (string.IsNullOrEmpty(userEmail))
+            //{
+            //    Console.WriteLine("User.Identity.Name è null o vuoto");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"User.Identity.Name: {userEmail}");
+            //}
 
-            var user = await _dbContext.Users
-                .Where(x => x.Email == userEmail)
-                .Select(x => x.Id)
-                .FirstOrDefaultAsync();
+            //var user = await _dbContext.Users
+            //    .Where(x => x.Email == userEmail)
+            //    .Select(x => x.Id)
+            //    .FirstOrDefaultAsync();
 
-            if (user == null)
-            {
-                return NotFound("Utente non trovato");
-            }
+            //if (user == null)
+            //{
+            //    return NotFound("Utente non trovato");
+            //}
 
-            // Retrieves only the completed user task from history
-            var allData = await _dbContext.Users
-                .Where(x => x.Email == userEmail)
-                .Select(x => new
-                {
-                    x.Id,
-                    x.FirstName,
-                    x.Role,
-                    Events = x.Events
-                        .Where(e => e.Stato == "Completato")  // Filter events where state is "Completato"
-                        .Select(e => new
-                        {
-                            e.Descrizione,
-                            e.Tipologia,                          
-                            e.Stato
-                        })
-                        .ToList()
-                })
-                .ToListAsync();
+            //// Retrieves only the completed user task from history
+            //var allData = await _dbContext.Users
+            //    .Where(x => x.Email == userEmail)
+            //    .Select(x => new
+            //    {
+            //        x.Id,
+            //        x.FirstName,
+            //        x.Role,
+            //        Events = x.Events
+            //            .Where(e => e.Stato == "Completato")  // Filter events where state is "Completato"
+            //            .Select(e => new
+            //            {
+            //                e.Descrizione,
+            //                e.Tipologia,                          
+            //                e.Stato
+            //            })
+            //            .ToList()
+            //    })
+            //    .ToListAsync();
 
-            if (allData == null || !allData.Any())
-            {
-                return NotFound("Nessun task completato trovato.");
-            }
+            //if (allData == null || !allData.Any())
+            //{
+            //    return NotFound("Nessun task completato trovato.");
+            //}
 
-            // Create the template for the view
-            var model = allData.Select(userData => new StoricoViewModel
-            {
-                Nome = userData.FirstName,               
-                Ruolo = userData.Role,
-                Email = userEmail,               
-                Events = userData.Events.Select(e => new Event
-                {
-                    Tipologia = e.Tipologia,
-                    Descrizione = e.Descrizione,
-                    Stato = e.Stato
-                }).ToList()
-            }).ToList();
+            //// Create the template for the view
+            //var model = allData.Select(userData => new StoricoViewModel
+            //{
+            //    Nome = userData.FirstName,               
+            //    Ruolo = userData.Role,
+            //    Email = userEmail,               
+            //    Events = userData.Events.Select(e => new Event
+            //    {
+            //        Tipologia = e.Tipologia,
+            //        Descrizione = e.Descrizione,
+            //        Stato = e.Stato
+            //    }).ToList()
+            //}).ToList();
 
-            return View(model);
+            List<StoricoViewModel> list = [new() { Descrizione = "Task 1", Tipologia = TipologiaEvento.Interna }, new() { Descrizione = "Task 2", Tipologia = TipologiaEvento.Esterna }];
+
+            return View(list);
         }
     }
 }
