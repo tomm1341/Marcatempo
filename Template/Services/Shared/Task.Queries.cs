ff9d807dd0e6b2545a326bf985857c47ab71bcd2
tomm1341 <tommaso.foca@studio.unibo.it>
2025-04-15 15:22:59 +0200

    Created query to get every task

---
+++ Template/Services/Shared/Task.Queries.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Services.Shared
+{
+    public class GetAllTasksQuery
+    {
+        public Guid Id { get; set; }
+
+        public Guid IdCreatore { get; set; } 
+        public string Titolo { get; set; }
+        public string Stato { get; set; }
+        public string Tipologia { get; set; }
+
+        public string Descrizione { get; set; }
+        public DateTime DataCreazione { get; set; }
+        public DateTime? DataScadenza { get; set; }
+    }
+    
+    public partial class SharedService
+    {
+        public async Task<List<Task>> GetAllTasksAsync() // Returns every task
+        {
+            return await _dbContext.Tasks
+                .OrderByDescending(t => t.DataCreazione)
+                .ToListAsync();
+        }
+    }
+    
+}
