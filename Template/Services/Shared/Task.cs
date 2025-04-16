8336ec12bc882c3c1a32dc331e94c9ee51023216
Tommaso Foca <tommaso.foca@vem.com>
2025-04-16 14:22:27 +0200

    Commit prova

--- Template/Services/Shared/Task.cs
+++ Template/Services/Shared/Task.cs
@@ -28,6 +28,8 @@ namespace Template.Services.Shared
         public string Tipologia { get; set;} // Interno, esterno
         public string Descrizione { get; set;}
 
+
+
         public DateTime DataCreazione { get; set;} // Optional
 
         public DateTime DataScadenza { get; set;} // Optional
