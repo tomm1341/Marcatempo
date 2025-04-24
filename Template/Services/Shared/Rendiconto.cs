f2a42c89da08137de72f6c1269fb51112488e7ed
tomm1341 <tommasofoca1@gmail.com>
2025-04-24 10:44:32 +0200

    Revisited Rendiconto table

--- Template/Services/Shared/Rendiconto.cs
+++ Template/Services/Shared/Rendiconto.cs
@@ -11,14 +11,20 @@ namespace Template.Services.Shared
         [Key]
         public Guid IdUtente { get; set; }
 
-        public Guid IdCommessa { get; set; }
+        public DateTime Data { get; set; } // Data del giorno corrente
 
-        public Guid IdTask {  get; set; }
+        public int OreLavorate { get; set; } // Ore lavorate in un giorno
 
-        public int Ore {  get; set; }
+        public int OraInizio { get; set; } // Ora di inizio lavoro, da passare in automatico lato frontend
 
-        public string Descrizione { get; set; }
+        public int OraFine { get; set; } // Ora di fine lavoro, da passare in automatico lato frontend
 
 
     }
 }
+
+
+/*
+ VALUTARE LA CREAZIONE DI UN'ALTRA TABELLA RENDICONTO_SETTIMANALE (O MENSILE AD ESEMPIO). RENDICONTO NORMALE TIENE CONTO DELLE ORE LAVORATE IN UN GIORNO,
+MENTRE QUELLO SETTIMANALE/MENSILE EFFETTUA IL CALCOLO DELLE ORE LAVORATE IN QUEL PERIODO, MAGARI ANCHE CALCOLANDO LA PAGA DOVUTA.
+ */
