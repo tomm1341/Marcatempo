f648a227839e3d95f97cb43c77d700f987de705b
tomm1341 <tommasofoca1@gmail.com>
2025-04-24 17:31:34 +0200

    Populated table

--- Template/Services/Shared/RendicontoSettimanale.cs
+++ Template/Services/Shared/RendicontoSettimanale.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Template.Services.Shared
 {
-    internal class RendicontoSettimanale
+    public class ResocontoSettimanale
     {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid IdUtente { get; set; }
+        [Required]
+        public int NumeroSettimana { get; set; } // ISO week number
+        public int OreTotaliLavorate { get; set; }
+        public int GiorniLavorati { get; set; }
+        [Required]
+        public DateTime DataInizioSettimana { get; set; }
+        [Required]
+        public DateTime DataFineSettimana { get; set; }
     }
+
 }
+
+
+//da valutare bene la logica
