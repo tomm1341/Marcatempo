582f06d8b44ede4d675d3f9ad631b5499d0b0007
AlexApo03 <alexandru.apostol@studio.unibo.it>
2025-04-28 11:43:00 +0200

    Aggiunta pagina dettagli

---
+++ Template.Web/Features/Dettagli/DettagliViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using Template.Services.Shared;
+
+namespace Template.Web.Features.Dettagli
+{
+    public class DettagliViewModel
+    {
+        public string Descrizione { get; set; } // Descrizione dell'evento
+
+        public string Priorità { get; set; } // Alta, Media o Bassa
+
+        public DateTime Scadenza { get; set; } // Data di scadenza
+
+        public string Stato { get; set; } // In Lavorazione o Completato
+    }
+}
