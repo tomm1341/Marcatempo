27eea96602df8d1e06775a9df5bd862f0f479376
tomm1341 <tommasofoca1@gmail.com>
2025-05-06 15:21:36 +0200

    Changed Dettagli page to view more info

--- Template.Web/Features/Dettagli/DettagliController.cs
+++ Template.Web/Features/Dettagli/DettagliController.cs
@@ -39,11 +39,34 @@ namespace Template.Web.Features.Dettagli
                     Priorità = dto.Priorità,
                     Creazione = dto.DataCreazione,
                     Scadenza = dto.DataScadenza,
-                    IdAssegnatario = dto.IdAssegnatario
+                    IdAssegnatario = dto.IdAssegnatario,
+                    IdCreatore = dto.IdCreatore,
+                    NomeCreatore = dto.NomeCreatore,
+
+                    IsOwner = dto.IdAssegnatario.HasValue && dto.IdAssegnatario.Value == CurrentUserId // Controlla se l'utente è owner del task
                 };
 
                 return View("~/Features/Dettagli/Dettagli.cshtml", vm);
             }
-        }
+        /* QUERY DI MODIFICA
+            [HttpPost]
+            public async Task<IActionResult> CompletaTask(DettagliViewModel model)
+            {
+                var task = await _dbContext.Tasks.FindAsync(model.TaskId);
+                if (task == null) return NotFound();
+
+                // Solo assegnatario può modificare
+                if (task.IdAssegnatario != UserId) return Forbid();
+
+                task.Descrizione = model.Descrizione;
+                task.OreLavorate = model.OreLavorate;
+                task.Stato = "Completato";
+
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Details", new { id = task.Id });
+            }*/
+
     }
+}
 
--- Template.Web/Features/Dettagli/DettagliViewModel.cs
+++ Template.Web/Features/Dettagli/DettagliViewModel.cs
@@ -8,15 +8,19 @@ namespace Template.Web.Features.Dettagli
     public class DettagliViewModel
     {
         public Guid TaskId { get; set; }
+        public Guid IdCreatore { get; set; }
+        public string NomeCreatore { get; set; }
         public Guid? IdAssegnatario { get; set; }
         public string Titolo { get; set; }
         public string Tipologia { get; set; }
-        public string Descrizione { get; set; } // Descrizione dell'evento
+        public string Descrizione { get; set; }
 
-        public string Priorità { get; set; } // Alta, Media o Bassa
+        public string Priorità { get; set; } 
         public DateTime Creazione { get; set; }
-        public DateTime Scadenza { get; set; } // Data di scadenza
+        public DateTime Scadenza { get; set; } 
+        public string Stato { get; set; } 
+        public string OreLavorate { get; set; }
 
-        public string Stato { get; set; } // In Lavorazione o Completato
+        public bool IsOwner { get; set; } // Utilizzato per gestione view
     }
 }
