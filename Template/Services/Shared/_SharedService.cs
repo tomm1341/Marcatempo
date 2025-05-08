ff7affb92ef4749f7570a614ce821fef953a52cb
tomm1341 <tommasofoca1@gmail.com>
2025-05-08 10:54:05 +0200

    Implemented code to handle Salva Modifiche button in Dettagli page

--- Template/Services/Shared/_SharedService.cs
+++ Template/Services/Shared/_SharedService.cs
@@ -1,12 +1,55 @@
-﻿namespace Template.Services.Shared
+﻿using System;
+using System.Threading.Tasks;
+using Template.Services.Shared;
+
+namespace Template.Services.Shared
 {
     public partial class SharedService
     {
-        TemplateDbContext _dbContext;
+        private readonly TemplateDbContext _dbContext;
 
         public SharedService(TemplateDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        // The following code is used to handle the 'Salva modifiche' button in Dettagli
+        public async Task<Guid?> UpdateTaskAndRendicontoAsync(
+            Guid taskId,
+            string nuovaDescrizione,
+            RendicontoDTO rendDto,
+            Guid currentUserId)
+        {
+            var task = await _dbContext.Tasks.FindAsync(taskId);
+            if (task == null)
+                throw new ArgumentException("Task non trovato");
+            if (task.IdAssegnatario != currentUserId)
+                throw new UnauthorizedAccessException("Non sei autorizzato");
+            task.Descrizione = nuovaDescrizione;
+            _dbContext.Tasks.Update(task);
+
+            Guid? nuovoRendId = null;
+            if (rendDto != null
+                && rendDto.OraFine > rendDto.OraInizio
+                && rendDto.Data.Date <= DateTime.Today)
+            {
+                var rend = new Rendiconto
+                {
+                    Id = Guid.NewGuid(),
+                    IdUtente = currentUserId,
+                    IdTask = taskId,
+                    OreLavorate = rendDto.OraFine - rendDto.OraInizio,
+                    Data = rendDto.Data.Date,
+                    OraInizio = rendDto.OraInizio,
+                    OraFine = rendDto.OraFine
+                };
+                nuovoRendId = rend.Id;
+                _dbContext.Rendiconto.Add(rend);
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return nuovoRendId;
         }
     }
 }
