54ce966b2791edd100c8451150fe9d512d142134
tomm1341 <tommasofoca1@gmail.com>
2025-05-08 10:56:02 +0200

    Created file and GetRendicontoByUser query

---
+++ Template/Services/Shared/Rendiconto.Queries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Template.Services.Shared
+{
+    public partial class SharedService
+    {
+        public async Task<IEnumerable<RendicontoDTO>> GetRendicontoByUserAsync(Guid userId)
+        {
+            return await _dbContext.Rendiconto
+                .Where(r => r.IdUtente == userId)
+                .Select(r => new RendicontoDTO
+                {
+                    IdRendiconto = r.Id,
+                    IdUtente = r.IdUtente,
+                    IdTask = r.IdTask,
+                    OreLavorate = r.OreLavorate,
+                    Data = r.Data,
+                    OraInizio = r.OraInizio,
+                    OraFine = r.OraFine
+                })
+                .ToListAsync();
+        }
+    }
+}
