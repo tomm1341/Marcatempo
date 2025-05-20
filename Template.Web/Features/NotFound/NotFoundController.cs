c9869c080f846e2df70bbc2dc927a0720f0fa803
tomm1341 <tommasofoca1@gmail.com>
2025-05-20 15:12:47 +0200

    Created NotFound page

---
+++ Template.Web/Features/NotFound/NotFoundController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Web.Features.NotFound
+{
+    public partial class NotFoundController : Controller
+    {
+        [Route("NotFound")]
+        public IActionResult NotFound(int statusCode = 404)
+        {
+            // Imposta 404 anche nella response (già automatico, ma per sicurezza)
+            Response.StatusCode = statusCode;
+            return View("NotFound");
+        }
+    }
+}
